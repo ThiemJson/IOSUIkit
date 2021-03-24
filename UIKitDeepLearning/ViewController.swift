d1d88127ee97e689b91b2058e240469bc0aea574
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-24 16:20:25 +0700

    use Button UIVIew

--- UIKitDeepLearning/ViewController.swift
+++ UIKitDeepLearning/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var alertController : UIAlertController = UIAlertController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        self.alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(self.alertController, animated: true, completion: nil)
+//        print("clicked")
+    }
+    @IBAction func showMessageSecond(_ sender: UIButton) {
+        self.alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(self.alertController, animated: true, completion: nil)
+    }
 }
 
