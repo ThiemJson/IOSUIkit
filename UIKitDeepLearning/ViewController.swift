5c8866ecf81bddd94a11207bbba0405a6fe8e482
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-24 15:03:32 +0700

    Initial Commit

---
+++ UIKitDeepLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDeepLearning
+//
+//  Created by Thiem Jason on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
