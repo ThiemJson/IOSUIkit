9c50f8bb1e7ecd66010d27f011b742f06c48add3
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-26 01:02:42 +0700

    Try stackview

--- UIKitDeepLearning/ViewController.swift
+++ UIKitDeepLearning/ViewController.swift
@@ -11,19 +11,6 @@ class ViewController: UIViewController {
     var alertController : UIAlertController = UIAlertController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func showMessage(sender: UIButton) {
-        self.alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        self.present(self.alertController, animated: true, completion: nil)
-//        print("clicked")
-    }
-    @IBAction func showMessageSecond(_ sender: UIButton) {
-        self.alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        self.present(self.alertController, animated: true, completion: nil)
     }
 }
 
