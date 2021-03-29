6dc546c0c0d4be3aa6ee9446770e1cd6ce1b4b9c
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2021-03-29 15:51:18 +0700

    Something with custom table view

---
+++ UIKitDeepLearning/RestaurantTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantTableViewCell.swift
+//  UIKitDeepLearning
+//
+//  Created by Thiem Jason on 3/29/21.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
